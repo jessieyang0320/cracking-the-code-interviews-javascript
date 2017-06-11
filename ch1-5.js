3e5c014e7d39bd9a38517c0089629908ea3f2b65
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-06-10 21:26:06 -0400

    ch1-5

---
+++ ch1-5.js
@@ -0,0 +1,25 @@
+// ch1-5 One Away
+// 3 edits: insert a char, delete a char, replace a char
+// given 2 strings, write a function to see if they are one edit away
+
+// step 1: without space, compare length: 
+// length1 = length2+1 || length1 == length 2 
+//  
+// step 2: compare chars 
+// 
+
+var checkMissing = function(str1,str2){
+	var length1 = str1.length;
+	var length2 = str2.length;
+
+	if (length1 !== length2 + 1){
+		return false 
+	} else {
+		if()
+	}
+}
+
+var checkDiff = function(str1,str2){
+
+}
+
---
+++ ch1-6.js
@@ -0,0 +1,4 @@
+// ch1-6 string compression 
+// compress a string as required and see if the length is smaller, if not print the original string
+// aaabbczzz => a3b2c1z3
+
